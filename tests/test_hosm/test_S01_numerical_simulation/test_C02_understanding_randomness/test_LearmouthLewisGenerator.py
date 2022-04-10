5fa6fc1e99bac91d4f574f9bba435016428cea44
wilsonify <tom.a.wilson@gmail.com>
2022-04-10 09:26:05 -0400

    better test for LLG

--- tests/test_hosm/test_S01_numerical_simulation/test_C02_understanding_randomness/test_LearmouthLewisGenerator.py
+++ tests/test_hosm/test_S01_numerical_simulation/test_C02_understanding_randomness/test_LearmouthLewisGenerator.py
@@ -1,15 +1,17 @@
-import numpy as np
+from inspect import getmembers, isfunction
+
+from hosm.S01_numerical_simulation.C02_understanding_randomness import LearmouthLewisGenerator
+from hosm.S01_numerical_simulation.C02_understanding_randomness.LearmouthLewisGenerator import LLG
+
 
 def test_smoke():
     print("fire?")
+    print(getmembers(LearmouthLewisGenerator, isfunction))
 
-def test_llg():    
-    a = 75
-    c = 0
-    m = 2**(31) -1
-    x  = 0.1
-    
-    for i in range(1,100):
-        x= np.mod((a*x+c),m)
-        u = x/m
-        print(u)
+
+def test_llg():
+    result = []
+    generator = LLG()
+    for i in range(1, 10):
+        result += [round(next(generator), 4)]
+    assert result == [0.0, 0.0, 0.0, 0.0015, 0.1105, 0.2878, 0.5828, 0.7089, 0.1686]
